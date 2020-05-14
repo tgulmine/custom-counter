6c391cc26f41db460198826e22fc601bd5cfb67c
tgulmine <tgulmine@gmail.com>
2020-05-14 20:00:46 -0300

    add basic colors

---
+++ src/components/ColorBox/ColorBox.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import './ColorBox.scss';
+
+interface ColorBoxProps {
+  color: string;
+}
+
+const ColorBox: React.FC<ColorBoxProps> = props => {
+  var color = props.color;
+
+  return (
+    <div
+      className="rounded p-3 bg-gray-800 ml-2 mb-2 border-white border-1"
+      style={{
+        backgroundColor: color
+      }}
+    />
+  );
+};
+
+export default ColorBox;
--- src/components/CreateCounter/CreateCounter.tsx
+++ src/components/CreateCounter/CreateCounter.tsx
@@ -1,21 +1,37 @@
 import * as React from 'react';
 import './CreateCounter.scss';
+import ColorBox from '../ColorBox/ColorBox';
 
 const CreateCounter: React.FC = () => {
+  const colorList = [
+    '#1A1333',
+    '#262949',
+    '#045459',
+    '#087353',
+    '#15C286',
+    '#ABD96D',
+    '#FBBF54',
+    '#EE6B3B',
+    '#EC0F47',
+    '#A02C5D',
+    '#700460',
+    '#022C7A'
+  ];
+
   return (
-    <div className="flex-row ml-4 w-1/4 bg-red-300 rounded p-4 mt-4">
-      <div className="font-bold text-2xl w-1/2 text-black mr-auto ml-auto">New Counter</div>
-      <div className="flex justify-around items-center mr-2">
-        <div className="text-black text-lg">Name</div>
+    <div className="flex-row ml-4 w-1/5 bg-red-300 rounded p-4 mt-4">
+      <div className="font-bold text-2xl w-full text-black mr-auto ml-auto">New Counter</div>
+      <div className="flex items-center mr-2 mt-2">
+        <div className="text-black text-lg w-1/3">Name</div>
         <input
-          className="shadow appearance-none border rounded w-1/2 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          className="shadow appearance-none border rounded w-2/3 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
           id="title"
           type="text"
           placeholder="Name"
         />
       </div>
-      <div className="flex justify-around items-center mr-2">
-        <div className="text-black text-lg">Time</div>
+      <div className="flex items-center mr-2 mt-2">
+        <div className="text-black text-lg w-1/3">Time</div>
         <input
           className="shadow appearance-none border rounded w-1/2 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
           id="time"
@@ -23,10 +39,13 @@ const CreateCounter: React.FC = () => {
           placeholder="Time"
         />
       </div>
-      <div className="flex">
-        <div className="text-black text-lg">Color</div>
+      <div className="flex mt-2">
+        <div className="text-black text-lg w-1/3">Color</div>
+        <div className="flex flex-wrap justify-between">
+          {colorList && colorList.map((color, index) => <ColorBox color={colorList[index]} />)}
+        </div>
       </div>
-      <div className="flex">
+      <div className="flex mt-2">
         <button
           className="bg-black hover:bg-gray-200 hover:text-black text-white text-xl font-bold mr-auto ml-auto py-2 px-4 rounded focus:outline-none focus:shadow-outline"
           type="button"
